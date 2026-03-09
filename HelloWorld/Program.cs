53eafaecf5d33a6931ff05ee937781e10ca2a3f5
panamertikas <mertikas91@gmail.com>
2026-03-09 19:35:18 +0200

    chore: add basic output logic

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
+
 {
+    /// <summary>
+    /// This is the first program in C# that prints "Hello, World!" to the console.
+    /// It serves as a basic example of a C# application and demonstrates the structure of a C# program,
+    /// including the use of namespaces, classes, and the Main method as the entry point of the application.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
         }
     }
 }
