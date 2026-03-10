515382280f1975d78d0e49bb5c5c986fe8e41096
panamertikas <mertikas91@gmail.com>
2026-03-10 20:20:11 +0200

    feat : add expression - bodied methods

---
+++ ExpressionBodyMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpressionBodyMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        // Expression bodied method - a more concise way to write method
+        // s that consist of a single expression
+        public static int Add(int a, int b) => a + b;
+
+        public static int Factorial(int n) => (n == 0) ? 1 : n * Factorial(n - 1);
+
+        public static bool IsEven(int n) => n % 2 == 0;
+    }
+}
